c0c7912998a8fb6a659e6728356ca4389f440587
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-05-07 11:01:42 +0300

    Renamed filmContext -> movieContext

--- Filmiregister/Filmiregister/DatabaseContext/FilmContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Filmiregister.DatabaseContext
-{
-    public class FilmContext : DbContext
-    {
-        public FilmContext(DbContextOptions<FilmContext> options) : base(options) { }
-    }
-}
