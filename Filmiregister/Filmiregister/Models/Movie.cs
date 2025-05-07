02d368af9d446a43b67289613f92ed749d9b5064
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-05-07 11:00:35 +0300

    Added movie model

---
+++ Filmiregister/Filmiregister/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Filmiregister.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieDescription { get; set; }
+        public string MovieImage { get; set; }
+        public string MovieSecondImage { get; set; }
+        public int MovieRating { get; set; }
+        public List<string> Categories { get; set; }
+        public DateOnly PublicationDate { get; set; }
+        public string MovieLanguage { get; set; }
+        //public List<Account> UsersWhoOwnMovie { get; set; }
+        //public List<Comment> CommentsOnMovie { get; set; }
+    }
+}
