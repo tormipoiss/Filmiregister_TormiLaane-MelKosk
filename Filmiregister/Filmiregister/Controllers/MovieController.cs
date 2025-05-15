5cc290e785db606f92eac426afd648b564be3b0c
mel2134 <158561962+mel2134@users.noreply.github.com>
2025-05-15 16:26:52 +0300

    https://tormipoiss.atlassian.net/jira/software/projects/FTLMK/boards/34?selectedIssue=FTLMK-7
    Resolves all tasks

---
+++ Filmiregister/Filmiregister/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using Filmiregister.DatabaseContext;
+using Filmiregister.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Filmiregister.Controllers
+{
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private readonly MovieContext _context;
+        public MovieController(MovieContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return RedirectToAction("Index","Home");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null) return RedirectToAction("Index", "Home");
+
+            var movie = _context.Movies.FirstOrDefault(m=>m.MovieID == id);
+
+            if (movie == null) return RedirectToAction("Index", "Home");
+
+            return View(movie);
+        }
+    }
+}
--- Filmiregister/Filmiregister/DatabaseContext/DbInitializer.cs
+++ Filmiregister/Filmiregister/DatabaseContext/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Filmiregister.Models;
+using System.Globalization;
 
 namespace Filmiregister.DatabaseContext
 {
@@ -10,22 +11,34 @@ namespace Filmiregister.DatabaseContext
 
             var movies = new Movie[]
             {
-                new Movie()
+                new Movie
                 {
-                    MovieID = 1,
-                    MovieTitle = "",
-                    MovieDescription = "",
-                    MovieImage = "",
-                    MovieSecondImage = "",
-                    MovieRating = 1,
-                    Categories = new() {"",""},
-                    PublicationDate = new(),
+                    MovieTitle = "Title",
+                    MovieDescription = "Description",
+                    MovieImage = "https://placehold.co/500x750.png",
+                    MovieSecondImage = "https://placehold.co/1280x720.png",
+                    MovieRating = 5,
+                    Categories = new List<string> { "drama", "action" },
+                    PublicationDate =  DateOnly.FromDateTime(DateTime.UtcNow),
                     MovieLanguage = "en",
-                    Duration = new()
-
+                    Duration = new TimeOnly(2, 0)
+                },
+                new Movie // pole film
+                {
+                    MovieTitle = "The Flash",
+                    MovieDescription = "After being struck by lightning, CSI investigator Barry Allen awakens from a nine-month coma to discover he has been granted the gift of super speed. Teaming up with S.T.A.R. Labs, Barry takes on the persona of The Flash, the Fastest Man Alive, to protect his city.",
+                    MovieImage = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yZevl2vHQgmosfwUdVNzviIfaWS.jpg",
+                    MovieSecondImage = "https://media.themoviedb.org/t/p/w533_and_h300_bestv2/gFkHcIh7iE5G0oVOgpmY8ONQjhl.jpg",
+                    MovieRating = 4,
+                    Categories = new List<string> { "Drama", "Action" },
+                    PublicationDate = DateOnly.ParseExact("14/10/2014", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    MovieLanguage = "en",
+                    Duration = new TimeOnly(2, 30)
                 }
 
             };
+            context.Movies.AddRange(movies);
+            context.SaveChanges();
         }
     }
 }
